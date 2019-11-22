42720acd84baf2705b2d1504104657525520b773
Diego Rossi <diego.rossi@yahoo.com.br>
2019-11-22 00:05:36 -0300

    Update Conexao.php

--- Conexao.php
+++ Conexao.php
@@ -1,7 +1,7 @@
 <?php
-	$Servidor = 'sql50.main-hosting.eu';
-	$Usuario = 'u980558249_admin';
-	$Senha = '0w9u9fsoh';
-	$Banco = 'u980558249_cons';
+	$Servidor = 'server';
+	$Usuario = 'user';
+	$Senha = 'pass';
+	$Banco = 'bdname';
 	$conexao = new mysqli($Servidor,$Usuario,$Senha,$Banco);
 ?>
